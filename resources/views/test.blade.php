abdb9335c3504fb5f1cc982e1a01b999f7c0bcbc
Kukuh182 <kukuhpr21@gmail.com>
2025-07-15 23:16:46 +0700

    remove gitignore files

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body class="bg-amber-300 p-3">
+    <h1 class="text-teal-500">kdjfhgkdfjh</h1>
+</body>
+</html>
