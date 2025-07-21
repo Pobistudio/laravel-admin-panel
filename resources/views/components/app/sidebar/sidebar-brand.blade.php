02f7be9c91e26603587946033158ab2ad2c3078b
Kukuh182 <kukuhpr21@gmail.com>
2025-07-21 22:47:59 +0700

    update sidebar brand

--- resources/views/components/app/sidebar/sidebar-brand.blade.php
+++ resources/views/components/app/sidebar/sidebar-brand.blade.php
@@ -1,3 +1,4 @@
-<div class="p-4 bg-gradient-to-r from-lap-brown via-lap-peach to-lap-cream sticky top-0 z-10">
+<div class=" flex flex-row p-4 gap-3  items-center justify-center ticky top-0 z-10">
+    <img src="{{ asset('assets/images/logo.png') }}" alt="logo" class="w-10 h-10 rounded-full">
     <h2 class="text-xl font-bold mb-0">Admin Panel</h2>
 </div>
