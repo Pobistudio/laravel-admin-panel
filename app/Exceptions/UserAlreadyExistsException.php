8e4d830b0930e30b00ef77c5a97c534bd09fd971
kukuhpr21 <kukuhpr21@gmail.com>
2025-07-24 15:38:34 +0700

    add new exception

---
+++ app/Exceptions/UserAlreadyExistsException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Throwable;
+
+class UserAlreadyExistsException extends Exception
+{
+     public function __construct(string $message = "User already exists.", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
