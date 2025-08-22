e9ed1d9fd57102923c26c4cf772a49b53c053e61
kukuhpr21 <kukuhpr21@gmail.com>
2025-08-22 15:59:36 +0700

    add laravel datatables vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@ import './alert.js';
 import './dialog.js';
 import './sidebar.js';
 import './dropdown-menu-navbar.js';
+import 'laravel-datatables-vite';
+import './datatable-tailwind.js';
 
 window.confirmLogoutDialog = function (logoutUrl) {
     showConfirmDialog('Logout', 'Apakah anda ingin logout aplikasi ini ?', 'Logout', function () {
