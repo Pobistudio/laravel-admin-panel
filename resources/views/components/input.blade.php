0eda78669598e7a3377d12507b8f4f97f29a579d
kukuhpr21 <kukuhpr21@gmail.com>
2025-09-30 14:59:37 +0700

    update input component

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -3,12 +3,12 @@
     $classes = "p-3 w-full bg-slate-200 border border-slate-300 rounded-lg focus:outline-slate-400";
 @endphp
 <div class="flex flex-col gap-1 w-full">
-    <div class="flex flex-row gap-1">
-        <input {{ $attributes->merge(['class' => "{$classes}", 'name' => $name]) }}>
+    <div class="relative w-full">
+        <input {{ $attributes->merge(['class' => "{$classes}" . ($isPassword ? ' pr-12' : ''), 'name' => $name]) }}>
         @if ($isPassword)
-            <div class="flex w-[10%] items-center justify-center cursor-pointer">
-                <i id="{{ $name }}_open_eye" onclick="togglePassword('{{ $name }}')" class="ri-eye-line"></i>
-                <i id="{{ $name }}_close_eye" onclick="togglePassword('{{ $name }}')" class="ri-eye-close-line hidden"></i>
+            <div class="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer">
+                <i id="{{ $name }}_open_eye" onclick="togglePassword('{{ $name }}')" class="ri-eye-line text-slate-500 hover:text-slate-700 transition-colors"></i>
+                <i id="{{ $name }}_close_eye" onclick="togglePassword('{{ $name }}')" class="ri-eye-close-line text-slate-500 hover:text-slate-700 transition-colors hidden"></i>
             </div>
         @endif
     </div>
