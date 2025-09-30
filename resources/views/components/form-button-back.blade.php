edc1b1e2ff602854eec2b26b6b44147671d4a388
kukuhpr21 <kukuhpr21@gmail.com>
2025-09-30 14:53:01 +0700

    add form button back component

---
+++ resources/views/components/form-button-back.blade.php
@@ -0,0 +1,4 @@
+@props(['action'])
+<div class="flex text-lap-navy hover:rounded-full hover:text-white hover:bg-slate-500 hover:drop-shadow w-9 h-9 items-center justify-center cursor-pointer">
+    <a href="{{ $action }}"><i class="ri-arrow-left-line"></i></a>
+</div>
