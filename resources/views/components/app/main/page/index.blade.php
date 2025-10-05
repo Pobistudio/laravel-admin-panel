c30bb04ff1fa7b820ee4e4a9e6e290c7e7eb8514
Kukuh182 <kukuhpr21@gmail.com>
2025-10-05 11:42:59 +0700

    update component page

--- resources/views/components/app/main/page/index.blade.php
+++ resources/views/components/app/main/page/index.blade.php
@@ -1,4 +1,4 @@
-<section class="flex flex-col w-full h-ful overflow-hidden text-lap-dark">
+<section class="flex flex-col md:pl-[250px] w-full h-screen overflow-hidden text-lap-dark">
     @include('components.app.main.page.page-header')
     @include('components.app.main.page.page-content')
 </section>
