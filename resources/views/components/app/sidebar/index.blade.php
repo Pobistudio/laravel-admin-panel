4692af772487d456a2d3785d4d821af93bb67e3a
Kukuh182 <kukuhpr21@gmail.com>
2025-10-05 11:43:12 +0700

    update component sidebar

--- resources/views/components/app/sidebar/index.blade.php
+++ resources/views/components/app/sidebar/index.blade.php
@@ -1,4 +1,4 @@
-<aside id="sidebar" class="flex flex-col w-64 h-full bg-white flex-shrink-0 overflow-y-auto transition-all duration-300 ease-in-out fixed inset-y-0 left-0 transform -translate-x-full z-50 md:relative md:translate-x-0">
+<aside id="sidebar" class="flex flex-col w-64 h-full bg-white flex-shrink-0 overflow-y-auto transition-all duration-300 ease-in-out fixed inset-y-0 left-0 transform -translate-x-full z-50 md:translate-x-0">
     <x-app.sidebar.sidebar-brand/>
     <x-app.sidebar.sidebar-menu/>
 </aside>
