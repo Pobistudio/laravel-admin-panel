17c1066256f06f794b22172bdbc91e50b9c464fd
Kukuh182 <kukuhpr21@gmail.com>
2025-10-13 08:46:05 +0700

    update route back button form

--- resources/views/pages/settings/statuses/create.blade.php
+++ resources/views/pages/settings/statuses/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title', "Create Status")
 @section('page')
-<x-form x-data="{ open: false}" method="POST" action="{{ route('statuses-create') }}" class="sm:w-1/2 w-full" :border="true" :actionBack="route('users')">
+<x-form x-data="{ open: false}" method="POST" action="{{ route('statuses-create') }}" class="sm:w-1/2 w-full" :border="true" :actionBack="route('statuses')">
     <x-label id="label_name" for="email">Name</x-label>
     <x-input type="text" id="name" name="name" value="{{ old('name') }}"/>
     <div class="flex justify-end w-full">
