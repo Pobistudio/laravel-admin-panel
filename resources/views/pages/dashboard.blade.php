c906db6e8058ea7fc0de96f9374abe77651f8b9c
kukuhpr21 <kukuhpr21@gmail.com>
2025-10-22 10:40:59 +0700

    update sub title dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title', "Dashboard")
 @section('page_title', "Welcome back")
-@section('page_subtitle', "Welcome to dashboard")
+@section('page_subtitle', "Welcome to ".config('app.name'))
 @section('page')
 <div class="grid grid-cols-1 md:grid-cols-5">
     @foreach ($dashboardData as $item)
