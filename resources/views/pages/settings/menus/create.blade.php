8db665ba82ce5cbad87f11417864ad7d9e14b6fb
Kukuh182 <kukuhpr21@gmail.com>
2025-10-22 08:46:32 +0700

    update form create menu

--- resources/views/pages/settings/menus/create.blade.php
+++ resources/views/pages/settings/menus/create.blade.php
@@ -6,6 +6,26 @@
     <x-select name="menu"  :options="$menus" value="{{ old('menu') }}" description="Pilih Default Parent untuk membuat Menu Parent"/>
     <x-label id="label_name" for="name">Name</x-label>
     <x-input type="text" id="name" name="name" value="{{ old('name') }}"/>
+    <div class="flex flex-col sm:flex-row w-full gap-4">
+        <div class="flex flex-col w-full sm:w-1/2 gap-4">
+            <x-label id="label_link" for="link">Link</x-label>
+            <x-input type="text" id="link" name="link" value="{{ old('link') }}" description="Default value '#'"/>
+        </div>
+        <div class="flex flex-col w-full sm:w-1/2 gap-4">
+            <x-label id="label_link_alias" for="link_alias">Link alias</x-label>
+            <x-input type="text" id="link_alias" name="link_alias" value="{{ old('link_alias') }}" description="Default value '#'"/>
+        </div>
+    </div>
+    <div class="flex flex-col sm:flex-row w-full gap-4">
+        <div class="flex flex-col w-full sm:w-1/2 gap-4">
+            <x-label id="label_icon" for="icon">Icon</x-label>
+            <x-input type="text" id="icon" name="icon" value="{{ old('icon') }}" description="Default value '#'"/>
+        </div>
+        <div class="flex flex-col w-full sm:w-1/2 gap-4">
+            <x-label id="label_order" for="order">Order</x-label>
+            <x-input type="text" id="order" name="order" value="{{ old('order') }}" description="Default value '0'"/>
+        </div>
+    </div>
     <div class="flex justify-end w-full">
         <x-button x-on:click="open = true" type="submit" class="flex items-center justify-center">
             <x-loader-button x-show="open"/>
