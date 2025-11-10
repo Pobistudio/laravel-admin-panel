8301ed1ffaf6e951917ece90b306aaa401014a1a
Kukuh182 <kukuhpr21@gmail.com>
2025-11-10 08:17:27 +0700

    init assign menu permission

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Menu;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class MenuSeeder extends Seeder
--- resources/views/pages/settings/menus/list.blade.php
+++ resources/views/pages/settings/menus/list.blade.php
@@ -6,7 +6,7 @@
             <x-tree.tree-menu/>
         </x-collapse>
         @section('additional_table_button')
-            <a href="#" class="p-3 bg-teal-500 text-white text-sm font-normal rounded-lg hover:bg-teal-700 hover:drop-shadow-2xl transition-all delay-150 cursor-pointer">Assign Permission</a>
+            <a href="#" class="p-3 bg-teal-500 text-white text-sm font-normal rounded-lg hover:bg-teal-700 hover:drop-shadow-2xl transition-all delay-150 cursor-pointer">Assign Menu Permission</a>
         @endsection
         <x-table :data="$dataTable" :routeButtonAdd="route('menus-create')"/>
     </div>
